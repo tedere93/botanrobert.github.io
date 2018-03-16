d2bbc67ec48be4c87dfc24cf65283f8d387c2287
botanrobert <botanrobert@gmail.com>
2018-03-16 18:51:38 +0200

    adaugare grade

--- x3d/orientare.js
+++ x3d/orientare.js
@@ -4,4 +4,7 @@ function on_device_orientation(e)
 {
 	document.getElementById("id_orientation").innerHTML = e.alpha + " " + e.beta + " " + e.gamma;
 
+	var viewpoint = document.getElementById("id_viewpoint");
+	viewpoint.setAttribute("orientation", "0 0 1" + e.alpha * Math.PI / 180);
+
 }
