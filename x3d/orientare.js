d8f8b7434c014d14e5a1ee84eca8c88334084321
botanrobert <botanrobert@gmail.com>
2018-03-16 18:31:33 +0200

    corectare orientare.js

--- x3d/orientare.js
+++ x3d/orientare.js
@@ -2,6 +2,6 @@ windows.addEventListener("deviceorientation", on_deviceorientation);
 
 function on_deviceorientation(e)
 {
-	document.getElementById("id_orintation").innerHTML = e.alpha + " " + e.beta + " " + e.gamma;
+	document.getElementById("id_orientation").innerHTML = e.alpha + " " + e.beta + " " + e.gamma;
 
 }
