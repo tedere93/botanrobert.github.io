9a6ed71de0d4d1ef6df837e309a59cacccf04687
botanrobert <botanrobert@gmail.com>
2018-03-16 19:30:40 +0200

    inversat z cu x

--- x3d/orientare_xz.js
+++ x3d/orientare_xz.js
@@ -9,8 +9,8 @@ function on_device_orientation(e)
 
 	var viewpoint = document.getElementById("id_viewpoint");
 
-	var x = radius * Math.cos (e.alpha * Math.PI / 180);
-	var z = radius * Math.sin (e.alpha * Math.PI / 180);
+	var z = radius * Math.cos (e.alpha * Math.PI / 180);
+	var x = radius * Math.sin (e.alpha * Math.PI / 180);
 	viewpoint.setAttribute("position", x + " 0 " + z);
 	viewpoint.setAttribute("orientation", "0 1 0 " + e.alpha * Math.PI / 180);
 
