bb258c9b7494f0d76c015a06e1a6551f106b0f66
botanrobert <botanrobert@gmail.com>
2018-03-16 19:18:28 +0200

    orientation_xz

---
+++ x3d/orientare_xz.js
@@ -0,0 +1,17 @@
+window.addEventListener("deviceorientation", on_device_orientation);
+
+
+var radius = 20;
+
+function on_device_orientation(e)
+{
+	document.getElementById("id_orientation").innerHTML = e.alpha.toFixed(1) + " " + e.beta.toFixed(1) + " " + e.gamma.toFixed(1);
+
+	var viewpoint = document.getElementById("id_viewpoint");
+
+	var x = radius * Math.cos (e.alpha * Math.PI / 180);
+	var z = radius * Math.sin (e.alpha * Math.PI / 180);
+	viewpoint.setAttribute("position", x + " 0 " + z);
+	viewpoint.setAttribute("orientation", "0 1 0 " + e.alpha * Math.PI / 180);
+
+}
